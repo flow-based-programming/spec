ac6062f0d257f7d637196da2b01ca078202686b7
Dan Lynch <pyramation@gmail.com>
2026-01-14 11:40:20 -0800

    nodes

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -2,32 +2,52 @@ export interface Metadata {
   x?: number;
   y?: number;
   description?: string;
+  [key: string]: any;
+}
+export interface Port {
+  name: string;
+  type: string;
+  description?: string;
+  optional?: boolean;
+}
+export interface Ref {
+  ref: string;
+  dialect?: string;
+}
+export interface Param {
+  name: string;
+  type: string;
+  value?: Ref | any;
+  description?: string;
 }
 export interface EdgeEndpoint {
   node: string;
   port: string;
 }
-export interface Value {
-  name: string;
-  type: string;
-  value?: any;
+export interface Edge {
+  src: EdgeEndpoint;
+  dst: EdgeEndpoint;
+  channel?: string;
+  meta?: Metadata;
 }
 export interface Group {
   name: string;
   nodes: string[];
   meta?: Metadata;
 }
+export type NodeKind = "node" | "subnet" | "graphInput" | "graphOutput";
 export interface Node {
   name: string;
+  kind?: NodeKind;
+  context: string;
+  category: string;
   type: string;
-  properties?: Value[];
-  inputs?: Value[];
-  outputs?: Value[];
-  edges?: Edge[];
-  nodes?: Node[];
+  typeVersion?: number;
   meta?: Metadata;
-}
-export interface Edge {
-  src: EdgeEndpoint;
-  dst: EdgeEndpoint;
+  inputs?: Port[];
+  outputs?: Port[];
+  params?: Param[];
+  nodes?: Node[];
+  edges?: Edge[];
+  groups?: Group[];
 }
